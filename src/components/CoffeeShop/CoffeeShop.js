10bd31de03520a7ee3e57e1363a5259a248bc5dd
cpv123 <cp.vibert@gmail.com>
2020-01-18 08:49:15 +0700

    Add star ratings to coffee shops

--- src/components/CoffeeShop/CoffeeShop.js
+++ src/components/CoffeeShop/CoffeeShop.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import StarRating from '../StarRating'
 
 function CoffeeShop({ shop }) {
   return (
     <div>
       <header>
         <h4>
-          {shop.name} • <small>{shop.location}</small>
+          {shop.name} • <small>{shop.location}</small> •{' '}
+          <StarRating rating={shop.rating} />
         </h4>
       </header>
     </div>
