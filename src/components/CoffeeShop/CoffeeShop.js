2aa2f79718666725a22d57b936df8370bbc3b506
cpv123 <cp.vibert@gmail.com>
2020-01-25 10:43:05 +0700

    Wrap shop rating on mobile screens

--- src/components/CoffeeShop/CoffeeShop.js
+++ src/components/CoffeeShop/CoffeeShop.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import StarRating from '../StarRating'
+import css from './CoffeeShop.module.css'
 
 function CoffeeShop({ shop }) {
   return (
-    <div>
-      <header>
-        <h4>
-          {shop.name} • <small>{shop.location}</small> •{' '}
-          <StarRating rating={shop.rating} />
-        </h4>
-      </header>
-    </div>
+    <h4 className={css.coffeeShopHeader}>
+      <span className={css.shopNameLocation}>
+        {shop.name} • <small>{shop.location}</small>
+      </span>
+      <span>
+        <span className={css.ratingDot}>•</span>{' '}
+        <StarRating rating={shop.rating} />
+      </span>
+    </h4>
   )
 }
 
