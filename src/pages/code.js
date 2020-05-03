9d5f005cfd3fec6f30469fec01f8f4b9882d582b
cpv123 <cp.vibert@gmail.com>
2020-05-03 17:46:16 +0700

    Add some code examples

--- src/pages/code.js
+++ src/pages/code.js
@@ -11,6 +11,21 @@ const CodePage = ({ location }) => {
     <Layout location={location}>
       <SEO title="Code" />
       <Bio bioLink={github} bioText="Some code" />
+      <ul>
+        <li>
+          <a href="https://github.com/cpv123">GitHub</a>
+        </li>
+        <li>
+          <a href="https://chrome.google.com/webstore/detail/githubgo/paojlcepodlafkpoecligghmbhjcmdhk">
+            Chrome extension: GitHubGo
+          </a>
+        </li>
+        <li>
+          <a href="https://chrome.google.com/webstore/detail/group-tabs/ohlcbhmckgkgegknphdfhoopdacaplbm">
+            Chrome extension: Group Tabs
+          </a>
+        </li>
+      </ul>
     </Layout>
   )
 }
