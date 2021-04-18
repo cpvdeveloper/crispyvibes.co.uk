3bd76b18fee5478fda19489d1192ee211f6eaf15
Chris Vibert <cp.vibert@gmail.com>
2021-04-18 15:29:32 +0700

    404 to TS

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import PageProps from 'gatsby'
 import Layout from '../components/Layout'
 import SEO from '../components/SEO'
 
-const NotFoundPage = ({ location }) => (
+const NotFoundPage = ({ location }: { location: PageProps['location'] }) => (
   <Layout location={location}>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
