14ca374ff052ddf92c7750fbe09e41cb1451b9f4
Chris Vibert <cp.vibert@gmail.com>
2021-04-18 10:07:23 +0700

    Remove unused components

--- src/components/ExtensionListItem/index.js
+++
@@ -1,2 +0,0 @@
-import { ExtensionListItem } from './ExtensionListItem'
-export default ExtensionListItem
--- src/pages/other-blog-add.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { AddForm } from '../components/ExternalBlogPosts/AddForm'
-
-const OtherBlogAdd = () => <AddForm />
-
-export default OtherBlogAdd
