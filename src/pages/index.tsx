150803132568ee5f0476ec2cb49ddc6c3e3781f0
Chris Vibert <cp.vibert@gmail.com>
2021-06-22 15:47:50 +0700

    Fix coffee homepage summary

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,7 +44,7 @@ export default function HomePage({ location, data, pageContext }: Props) {
   const { edges: topThreePosts } = data.allMarkdownRemark
   const recentCoffee = pageContext.coffeeShops
     .filter(shop => shop.location === 'Bangkok')
-    .slice(3)
+    .slice(0, 3)
   return (
     <Layout location={location}>
       <SEO title="Home" />
